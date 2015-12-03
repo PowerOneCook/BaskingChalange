0d823eb1db3ff69934accf55a9524aab89dbbdd6
PowerOneCook <bhb07@naver.com>
2015-12-03 20:15:01 +0900

    2015.12.03

--- app/src/main/java/com/swk/basket/wang/TeamActivity.java
+++ app/src/main/java/com/swk/basket/wang/TeamActivity.java
@@ -2,13 +2,89 @@ package com.swk.basket.wang;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class TeamActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_bar_team);
+        final CardView scoreDescBtn = (CardView)findViewById(R.id.team_score_desc);
+        final CardView scoreRootCard = (CardView)findViewById(R.id.team_score_desc_card);
+        scoreDescBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreRootCard.setVisibility(View.VISIBLE);
+                ScoreFragment scoreFragment = new ScoreFragment();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.team_score_desc_card,scoreFragment);
+                ft.commit();
+            }
+        });
     }
+    public static class ScoreFragment extends Fragment{
+        public ScoreFragment() {
+            super();
+        }
 
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v =  inflater.inflate(R.layout.fragment_team_score_desc,container,false);
+            RecyclerView scoreList = (RecyclerView)v.findViewById(R.id.frag_team_score_list);
+            RecyclerView.LayoutManager manager = new LinearLayoutManager(container.getContext(),LinearLayoutManager.HORIZONTAL,false);
+            scoreList.setLayoutManager(manager);
+            ScoreAdpater scoreAdpater = new ScoreAdpater();
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreAdpater.add("자살");
+            scoreList.setAdapter(scoreAdpater);
+            return v;
+        }
+    }
+    private static class ScoreAdpater extends RecyclerView.Adapter<ScoreAdpater.ViewHolder>{
+        static class ViewHolder extends RecyclerView.ViewHolder{
+            public ViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+        List<String> items = new ArrayList<>();
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+        public void add(String item){
+            items.add(item);
+            notifyDataSetChanged();
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.frag_team_score_item,parent,false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+    }
 }
--- app/src/main/java/com/swk/basket/wang/WaitActivity.java
+++ app/src/main/java/com/swk/basket/wang/WaitActivity.java
@@ -1,5 +1,6 @@
 package com.swk.basket.wang;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -9,10 +10,17 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class WaitActivity extends AppCompatActivity {
 
@@ -21,13 +29,19 @@ public class WaitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait);
         WaitFragment waitFragment = new WaitFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.wait_root, waitFragment);
         ft.commit();
         ViewPager pager = (ViewPager)findViewById(R.id.wait_pager);
         TabLayout tabs = (TabLayout)findViewById(R.id.wait_tab);
-        WaitPagerAdapter waitPagerAdapter = new WaitPagerAdapter(getSupportFragmentManager());
-        waitPagerAdapter.addFragment();
+
+        WaitPagerAdapter waitPagerAdapter = new WaitPagerAdapter(fm);
+        waitPagerAdapter.addFragment(new TalkFragment(),"카카오톡");
+        waitPagerAdapter.addFragment(new JoinPlayerFrgment(),"참여선수");
+        waitPagerAdapter.addFragment(new JoinPlayerFrgment(),"보류선수");
+        pager.setAdapter(waitPagerAdapter);
+        tabs.setupWithViewPager(pager);
 
     }
     public static class WaitFragment extends Fragment{
@@ -53,6 +67,8 @@ public class WaitActivity extends AppCompatActivity {
         ArrayList<String> titleList;
         public WaitPagerAdapter(FragmentManager fm) {
             super(fm);
+            fragList = new ArrayList<>();
+            titleList = new ArrayList<>();
         }
 
         @Override
@@ -74,17 +90,116 @@ public class WaitActivity extends AppCompatActivity {
             return titleList.get(position);
         }
     }
-    private class GameFragment extends Fragment{
-
-        public GameFragment() {
+    public static class TalkFragment extends Fragment{
+        private int tutorialIndex = 0;
+        int[] tutorials = new int[]{R.drawable.tutorial1,R.drawable.tutorial2};
+        ImageView tutorial;
+        public TalkFragment() {
             super();
+        }
+        public void getTutorial() {
+            tutorial.setImageResource( tutorials[tutorialIndex]);
+        }public void getTutorialnext() {
+            if(tutorialIndex < 1){
+                tutorialIndex++;
+            }
+            tutorial.setImageResource( tutorials[tutorialIndex]);
+        }public void getTutorialprev() {
+            if(tutorialIndex > 0){
+                tutorialIndex--;
+            }
+            tutorial.setImageResource( tutorials[tutorialIndex]);
         }
 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return null;
+            View v =  inflater.inflate(R.layout.fragment_wait_talk,container,false);
+
+            ImageButton nextBtn = (ImageButton)v.findViewById(R.id.frag_wait_talk_btn_tutorial_next);
+            ImageButton prevBtn = (ImageButton)v.findViewById(R.id.frag_wait_talk_btn_tutorial_prev);
+            tutorial = (ImageView)v.findViewById(R.id.frag_wait_talk_tutorial);
+            getTutorial();
+            nextBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getTutorialnext();
+                }
+            });
+            prevBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getTutorialprev();
+                }
+            });
+            return v;
         }
     }
+    public static class JoinPlayerFrgment extends Fragment{
+        List<String> players;
+        public JoinPlayerFrgment() {
+            super();
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            players = new ArrayList<>();
+        }
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_wait_join_player,container,false);
+            RecyclerView joinPlayerList = (RecyclerView)v.findViewById(R.id.frag_wait_join_player_list);
+            RecyclerView.LayoutManager manager = new LinearLayoutManager(container.getContext(),LinearLayoutManager.VERTICAL,false);
+            joinPlayerList.setLayoutManager(manager);
+            JoinPlayerAdapter joinPlayerAdapter = new JoinPlayerAdapter();
+            joinPlayerAdapter.add("정신병");
+            joinPlayerAdapter.add("정신병");
+            joinPlayerAdapter.add("정신병");
+            joinPlayerAdapter.add("정신병");
+            joinPlayerList.setAdapter(joinPlayerAdapter);
+            return v;
+        }
+    }
+    private static class JoinPlayerAdapter extends RecyclerView.Adapter<JoinPlayerAdapter.ViewHolder>{
+        static class ViewHolder extends RecyclerView.ViewHolder{
+            public ViewHolder(View itemView){
+                super(itemView);
+            }
+            AdapterView.OnItemClickListener mItemClickListner;
+
+            public void setOnItemClickListner(AdapterView.OnItemClickListener mItemClickListner) {
+                this.mItemClickListner = mItemClickListner;
+            }
+        }
+        List<String> items = new ArrayList<>();
+        AdapterView.OnItemClickListener mListener;
+        public void setOnItemClickListener(AdapterView.OnItemClickListener listener){
+            mListener = listener;
+        }
+        public void add(String item){
+            items.add(item);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.player_card,parent,false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.setOnItemClickListner(mListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+    }
+
+
 
 }
